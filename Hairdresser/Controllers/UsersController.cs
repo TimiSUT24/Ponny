b7150a3cacfb0c374ff1e6df5a1c27de54858eda
BingoDingo04 <viggo.kristensen@hotmail.com>
2025-05-13 14:17:33 +0200

    Ability to Register User

--- Hairdresser/Controllers/UsersController.cs
+++ Hairdresser/Controllers/UsersController.cs
@@ -1,6 +1,39 @@
-﻿namespace Hairdresser.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HairDresserClassLibrary.Models;
+using Hairdresser.Data;
+
+namespace Hairdresser.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Register User
+        [HttpPost("registerUser")]
+        public async Task<IActionResult> Register([FromBody] User newUser)
+        {
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
     }
 }
