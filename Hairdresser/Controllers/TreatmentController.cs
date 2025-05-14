16318b0c0d59442e8bd434c1ee946642eb69dc8b
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-05-14 13:47:46 +0200

    Added unittest project, added treatment controller but is not finished

---
+++ Hairdresser/Controllers/TreatmentController.cs
@@ -0,0 +1,70 @@
+﻿using Hairdresser.Repositories.Interfaces;
+using HairdresserClassLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Hairdresser.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Hairdresser.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TreatmentController : Controller
+    {
+        private readonly IGenericRepository<Treatment> _repository;
+
+        public TreatmentController(IGenericRepository<Treatment> repository)
+        {
+            _repository = repository;
+        }
+        [AllowAnonymous]
+        [HttpGet(Name = "GetAllTreatments")]
+        public async Task<IActionResult> GetAll()
+        {
+            var treatments = await _repository.GetAllAsync();
+            return Ok(treatments);
+        }
+
+        [Authorize]
+        [HttpPost(Name = "AddNewTreatment")]
+
+        public async Task<IActionResult> Create(string name, string description, int duration, double price  )
+        {
+            var treatment = new Treatment
+            {
+                Name = name,
+                Description = description,
+                Price = price,
+                Duration = duration,
+
+            };
+            await _repository.AddAsync(treatment);
+            await _repository.SaveChangesAsync();
+            return CreatedAtAction(nameof(Treatment), treatment);
+        }
+        [Authorize]
+        [HttpPut(Name = "")]
+        public async Task<IActionResult> Update(int id, [FromBody] Treatment treatment)
+        {
+            if(treatment.Id == id)
+            {
+                await _repository.UpdateAsync(treatment);
+                await _repository.SaveChangesAsync();
+            }
+           
+            return Ok("Treatment was updated");
+        }
+
+        [Authorize]
+        [HttpDelete(Name = "DeleteTreatment")]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var treatment = await _repository.GetByIdAsync(id); 
+
+            await _repository.DeleteAsync(treatment);
+            await _repository.SaveChangesAsync();
+            return Ok("Treatment was deleted"); 
+        }
+    }
+}
