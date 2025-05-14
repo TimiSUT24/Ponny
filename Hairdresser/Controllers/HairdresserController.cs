202e2e7dd6e431a652f98f4e24a4b551db936038
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-05-14 20:27:20 +0200

    Added hairdresser controller, with one HttpGet method

---
+++ Hairdresser/Controllers/HairdresserController.cs
@@ -0,0 +1,26 @@
+﻿using Hairdresser.Repositories.Interfaces;
+using HairdresserClassLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hairdresser.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HairdresserController : ControllerBase
+    {
+        private readonly IGenericRepository<ApplicationUser> _repository;
+
+        public HairdresserController(IGenericRepository<ApplicationUser> repository)
+        {
+            _repository = repository;
+        }
+        [AllowAnonymous]
+        [HttpGet(Name = "GetAllHairdressers")]
+        public async Task<IActionResult> GetAll()
+        {
+            var hairdressers = await _repository.GetAllAsync();
+            return Ok(hairdressers);
+        }
+    }
+}
