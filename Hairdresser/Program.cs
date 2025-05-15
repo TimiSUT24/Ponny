cd78bc25a051a59b6763862ec3e14a4014746020
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-05-15 18:12:00 +0200

    added missing authentication

--- Hairdresser/Program.cs
+++ Hairdresser/Program.cs
@@ -64,7 +64,7 @@ if (app.Environment.IsDevelopment())
 app.MapIdentityApi<ApplicationUser>();
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
