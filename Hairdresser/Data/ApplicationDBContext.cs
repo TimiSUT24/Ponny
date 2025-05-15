21f69557b7fa0599785d8ba9c06f66a5ec6c9397
BingoDingo04 <viggo.kristensen@hotmail.com>
2025-05-15 09:55:26 +0200

    Changed DBContext to use applicationUser

--- Hairdresser/Data/ApplicationDBContext.cs
+++ Hairdresser/Data/ApplicationDBContext.cs
@@ -1,18 +1,19 @@
 using HairdresserClassLibrary.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hairdresser.Data;
 
-public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : IdentityDbContext<IdentityUser>(options)
+public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<ApplicationUser> Users { get; set; } = null!;
     public DbSet<Treatment> Treatments { get; set; } = null!;
     public DbSet<Booking> Bookings { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        // Add any additional configuration here
+        // Här kan du lägga till konfigurationer om det behövs
     }
 }
