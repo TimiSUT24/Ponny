4891808b84342c2d6403b6e30508dd76a8c37bfa
HugoW5 <hugo05.wakt@gmail.com>
2025-05-16 11:21:38 +0200

    Add unit tests for TreatmentRepository functionality
    Added a new `TreatmentRepositoryTests` class in the `HairdresserUnitTests` namespace to test the `TreatmentRepository` class. Introduced necessary `using` directives for dependencies. Implemented `[TestInitialize]` and `[TestCleanup]` methods to configure and clean up an in-memory database for testing. Added a test method `Add_ShouldAddTreatmentSuccessfully` to verify treatments can be added to the repository.

---
+++ HairdresserUnitTests/TreatmentRepositoryTests.cs
@@ -0,0 +1,71 @@
+﻿using Hairdresser.Data;
+using Hairdresser.Repositories;
+using HairdresserClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairdresserUnitTests
+{
+	[TestClass]
+	public class TreatmentRepositoryTests
+	{
+		private ApplicationDBContext? _context;
+		private TreatmentRepository? _treatmentRepository;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			// Set up in-memory database
+			var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+				.UseInMemoryDatabase(Guid.NewGuid().ToString())
+				.Options;
+
+			_context = new ApplicationDBContext(options);
+			_treatmentRepository = new TreatmentRepository(_context);
+
+			// Save all changes
+			_context.SaveChanges();
+		}
+
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			_context!.Database.EnsureDeleted();
+			_context.Dispose();
+		}
+
+		[TestMethod]
+		public async Task Add_ShoudAddTreatmentSuccessfully()
+		{
+			//Arrage
+			var treatments = await _treatmentRepository!.GetAllAsync();
+
+			//Act
+			var newTreatment = new Treatment
+			{
+				Id = 1,
+				Name = "Haircut",
+				Price = 200
+			};
+
+			// Act
+			await _treatmentRepository!.AddAsync(newTreatment);
+			await _treatmentRepository.SaveChangesAsync();
+			var result = await _treatmentRepository.GetByIdAsync(1);
+
+			//Assert
+			Assert.IsNotNull(result);
+		}
+
+
+
+
+	}
+
+
+}
