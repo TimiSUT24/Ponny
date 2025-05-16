eae55152b4527ebfe7f4c27b6b1423168733617b
BingoDingo04 <viggo.kristensen@hotmail.com>
2025-05-16 09:58:37 +0200

    added get all treatments for customer

--- Hairdresser/Controllers/BookingsController.cs
+++ Hairdresser/Controllers/BookingsController.cs
@@ -1,6 +1,29 @@
-﻿namespace Hairdresser.Controllers
+﻿using HairdresserClassLibrary.Models;
+using Hairdresser.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hairdresser.Controllers
 {
-    public class BookingsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingsController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+
+        public BookingsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("treatments")]
+        public async Task<ActionResult<IEnumerable<Treatment>>> GetAllTreatments()
+        {
+            var treatments = await _context.Treatments
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(treatments);
+        }
     }
 }
