f016da47238dfd28f1b9f525ff229b33f0ca543e
HugoW5 <hugo05.wakt@gmail.com>
2025-05-22 14:10:12 +0200

    Test for conflic when booking an overlapping time

--- HairdresserUnitTests/BookingControllerTests.cs
+++ HairdresserUnitTests/BookingControllerTests.cs
@@ -2,6 +2,7 @@
 using Hairdresser.Data;
 using Hairdresser.DTOs;
 using Hairdresser.Repositories.Interfaces;
+using HairdresserClassLibrary.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -141,33 +142,58 @@ namespace HairdresserUnitTests
 		}
 
 
-		/*		[TestMethod]
-				public async Task Book_Occupied_Time_ReturnsConflict()
-				{
-					// Arrange
-					var bookingDTO = new BookingRequestDto
-					{
-						Start = DateTime.Now.AddDays(2),
-						TreatmentId = 5, // Non-existing treatment
-						HairdresserId = "1",
-						CustomerId = "1"
-					};
+		[TestMethod]
+		public async Task Book_Occupied_Time_ReturnsConflict()
+		{
+			// Arrange
+			_context.Bookings.Add(new Booking
+			{
+				Start = DateTime.Now.AddDays(2),
+				End = DateTime.Now.AddDays(2).AddMinutes(40),
+				HairdresserId = "1",
+				TreatmentId = 1,
+				CustomerId = "2"
+			});
+			_context.SaveChanges();
 
-					await _context.SaveChangesAsync();
 
-					// Act
-					var result = await _controller.BookAppointment(bookingDTO);
+			var customerId = "customer-1";
+			var hairdresserId = "hairdresser-1";
+			var treatmentId = 1;
+			var startTime = DateTime.Now.AddDays(2);
 
-					// Assert
-					if (result is NotFoundObjectResult notFoundResult)
-					{
-						Assert.AreEqual(404, notFoundResult.StatusCode);
-					}
-					else
-					{
-						Assert.Fail("Unexpected result type: " + result.GetType().Name);
-					}
-				}*/
+			var bookingDTO = new BookingRequestDto
+			{
+				Start = startTime,
+				TreatmentId = treatmentId,
+				HairdresserId = hairdresserId
+			};
+
+
+			// Mock authenticated user
+			var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+			{
+				new Claim(ClaimTypes.NameIdentifier, customerId)
+			}, "mock"));
+
+			_controller!.ControllerContext = new ControllerContext
+			{
+				HttpContext = new DefaultHttpContext { User = user }
+			};
+
+			// Act
+			var result = await _controller.BookAppointment(bookingDTO);
+
+			// Assert
+			if (result is ConflictObjectResult conflictResult)
+			{
+				Assert.AreEqual(409, conflictResult.StatusCode);
+			}
+			else
+			{
+				Assert.Fail("Unexpected result type: " + result.GetType().Name);
+			}
+		}
 
 		/*		[TestMethod]
 				public async Task Book_InvalidTime_ReturnsConflict()
