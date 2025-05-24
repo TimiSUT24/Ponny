2ae3d5e9dbebb9ea73b88bf7e89029f40c40a594
JosefForkman <44498331+JosefForkman@users.noreply.github.com>
2025-05-24 11:08:03 +0100

    add and update DTOs

--- Hairdresser/DTOs/TreatmentDTO.cs
+++ Hairdresser/DTOs/TreatmentDTO.cs
@@ -4,6 +4,7 @@ namespace Hairdresser.DTOs;
 
 public class TreatmentDTO
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Duration { get; set; }
     public double Price { get; set; }
