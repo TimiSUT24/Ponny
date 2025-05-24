6fbcf2c2f6f0ee33edfb1eb97d37b919d1027c75
BingoDingo04 <viggo.kristensen@hotmail.com>
2025-05-24 19:44:33 +0200

    Added ability for hairdresser to check monthly schedule

--- Hairdresser/Controllers/HairdresserController.cs
+++ Hairdresser/Controllers/HairdresserController.cs
@@ -56,6 +56,39 @@ namespace Hairdresser.Controllers
             return Ok(bookings);
         }
 
+        [Authorize(Roles = "Hairdresser")]
+        [HttpGet("monthly-schedule")]
+        public async Task<IActionResult> GetMonthlySchedule([FromQuery] string hairdresserId, [FromQuery] int year, [FromQuery] int month)
+        {
+            var monthStart = new DateTime(year, month, 1);
+            var monthEnd = monthStart.AddMonths(1);
+
+            var bookings = await _context.Bookings
+                .Where(b => b.HairdresserId == hairdresserId && b.Start >= monthStart && b.Start < monthEnd)
+                .Include(b => b.Customer)
+                .Include(b => b.Treatment)
+                .Include(b => b.Hairdresser)
+                .OrderBy(b => b.Start)
+                .ToListAsync();
+
+            var result = bookings.Select(b => new BookingResponseDto
+            {
+                Id = b.Id,
+                Start = b.Start,
+                End = b.End,
+                Treatment = b.Treatment,
+                UserDto = new UserDto
+                {
+                    UserName = b.Customer.UserName,
+                    Email = b.Customer.Email,
+                    PhoneNumber = b.Customer.PhoneNumber
+                },
+                Hairdresser = b.Hairdresser
+            });
+
+            return Ok(result);
+        }
+
         [Authorize(Roles = "Hairdresser,Admin")]
         [HttpGet("booking/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
