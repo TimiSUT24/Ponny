c467d58c49565116f26e05f21b9531ee4ca07d77
BingoDingo04 <viggo.kristensen@hotmail.com>
2025-05-26 14:26:51 +0200

    Added Tasks in IBookingRepo

--- Hairdresser/Repositories/Interfaces/IBookingRepository.cs
+++ Hairdresser/Repositories/Interfaces/IBookingRepository.cs
@@ -5,5 +5,9 @@ namespace Hairdresser.Repositories.Interfaces
     public interface IBookingRepository : IGenericRepository<Booking>
     {
         Task<Booking> GetByIdWithDetailsAsync(int id,string customerId);
+
+        Task<IEnumerable<Booking>> GetWeekScheduleWithDetailsAsync(string hairdresserId, DateTime weekStart);
+
+        Task<Booking?> GetBookingWithDetailsAsync(int bookingId);
     }
 }
