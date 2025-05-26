21a5690a8996b7ec89a9160faac850a3d6debd45
BingoDingo04 <viggo.kristensen@hotmail.com>
2025-05-26 14:29:12 +0200

    Added IHairdresserService

---
+++ Hairdresser/Repositories/Interfaces/IHairdresserService.cs
@@ -0,0 +1,16 @@
+﻿using Hairdresser.DTOs;
+using HairdresserClassLibrary.Models;
+
+namespace Hairdresser.Repositories.Interfaces
+{
+    public interface IHairdresserService
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllHairdressersAsync();
+
+        Task<IEnumerable<BookingResponseDto>> GetWeekScheduleAsync(string hairdresserId, DateTime weekStart);
+
+        Task<IEnumerable<BookingResponseDto>> GetMonthlyScheduleAsync(string hairdresserId, int year, int month);
+
+        Task<BookingResponseDto?> GetBookingDetailsAsync(int bookingId);
+    }
+}
