9327644af28ba46bea18f6cf7fa49458d6c8f3b7
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-05-26 15:37:37 +0200

    move BookingService in Program.cs

--- Hairdresser/Program.cs
+++ Hairdresser/Program.cs
@@ -68,11 +68,11 @@ builder.Services.AddAuthorization();
 builder.Services.AddScoped<IGenericRepository<Treatment>, TreatmentRepository>();
 builder.Services.AddScoped<IGenericRepository<Booking>, BookingRepository>();
 builder.Services.AddScoped<IGenericRepository<ApplicationUser>, UserRepository>();
-builder.Services.AddScoped<IBookingService, BookingService>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 //Services
+builder.Services.AddScoped<IBookingService, BookingService>();
 
 var app = builder.Build();
 
