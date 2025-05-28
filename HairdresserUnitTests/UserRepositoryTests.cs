e9d07d3f05d5a2bcf193713d1a5631e47382b950
HugoW5 <hugo05.wakt@gmail.com>
2025-05-28 13:19:23 +0200

    Add unit tests for UserRepository functionality
    Introduced `UserRepositoryTests` in `HairdresserUnitTests` namespace to test `UserRepository`. Added necessary `using` directives for required namespaces. Implemented `[TestInitialize]` method to set up an in-memory database and mock `UserManager<ApplicationUser>`. Created private fields for `ApplicationDBContext`, `Mock<UserManager<ApplicationUser>>`, and `UserRepository` to support testing.

---
+++ HairdresserUnitTests/UserRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using Hairdresser.Data;
+using Hairdresser.DTOs.User;
+using Hairdresser.Enums;
+using Hairdresser.Repositories;
+using HairdresserClassLibrary.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairdresserUnitTests
+{
+	[TestClass]
+	public class UserRepositoryTests
+	{
+		private ApplicationDBContext? _context;
+		private Mock<UserManager<ApplicationUser>> _userManagerMock = null!;
+		private UserRepository _userRepository = null!;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			// Set up in-memory database
+			var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+				.UseInMemoryDatabase(Guid.NewGuid().ToString())
+				.Options;
+
+			var store = new Mock<IUserStore<ApplicationUser>>();
+			store.As<IUserEmailStore<ApplicationUser>>(); // Required for FindByEmailAsync
+
+			_userManagerMock = new Mock<UserManager<ApplicationUser>>(
+				store.Object, null!, null!, null!, null!, null!, null!, null!, null!
+			);
+
+			_context = new ApplicationDBContext(options);
+			_userRepository = new UserRepository(_context, _userManagerMock.Object);
+		}
+	}
+}
